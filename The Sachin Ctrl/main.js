2646ebdbd31c0734752243bef606dc255b63359a
Sachin Kumar <96292115+sachin777s@users.noreply.github.com>
2022-10-11 10:28:06 +0530

    Update main.js

--- The Sachin Ctrl/main.js
+++ The Sachin Ctrl/main.js
@@ -2,7 +2,6 @@ let button = document.querySelectorAll("button");
 
 for (let i = 0; i < button.length; i++) {
     button[i].addEventListener("click",()=>{
-        button[i].style.border = "2px solid yellow";
+        button[i].style.border = "4px solid yellow";
     });
-   
 }
